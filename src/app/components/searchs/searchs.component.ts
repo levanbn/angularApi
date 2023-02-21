6714db88bb03561e9a17f636b584cdf6ad732646
levani <levancho.begashvili@gmail.com>
2023-02-21 22:57:43 +0400

    dada

--- src/app/components/searchs/searchs.component.ts
+++ src/app/components/searchs/searchs.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Country, movieInDetails} from 'src/app/movie.model'
-import { concat, forkJoin, map, Observable, reduce, switchMap } from 'rxjs';
+
+import { concat,tap, forkJoin, map, Observable, reduce, switchMap } from 'rxjs';
 import { MovieApiService } from 'src/app/movie-api.service';
 @Component({
   selector: 'app-searchs',
@@ -9,33 +10,33 @@ import { MovieApiService } from 'src/app/movie-api.service';
   styleUrls: ['./searchs.component.scss']
 })
 export class SearchsComponent implements OnInit {
-  first = new FormControl();
-  second = new FormControl();
-  third = new FormControl();
+  first : string = "";
+  second : string ="";
+  third : string = "";
   firstMovie$: Observable<movieInDetails> | undefined;
   secondMovie$: Observable<movieInDetails> | undefined;
   thirdMovie$: Observable<movieInDetails> | undefined;
   country$: Observable<Country> | undefined;
   population$: Observable<number> | undefined;
-
   minutesLength$: Observable<number> | undefined;
-
   constructor(private api: MovieApiService ) {}
 
   ngOnInit(): void {}
 
   send() {
-    this.firstMovie$ = this.api.getMovieDetails(this.first.value);
-    this.secondMovie$ = this.api.getMovieDetails(this.second.value);
-    this.thirdMovie$ = this.api.getMovieDetails(this.third.value);
+    this.firstMovie$ = this.api.getMovieDetails(this.first);
+    this.secondMovie$ = this.api.getMovieDetails(this.second);
+    this.thirdMovie$ = this.api.getMovieDetails(this.third);
     this.minutesLength$ = concat(
       this.firstMovie$,
       this.secondMovie$,
       this.thirdMovie$
     ).pipe(
+      tap((x) => console.log(x)),
       map((x) => Number(x.Runtime.split(' ')[0])),
       reduce((acc, curr) => acc + curr, 0)
     );
+    /*
     this.country$ = concat(
       this.firstMovie$,
       this.secondMovie$,
@@ -43,5 +44,7 @@ export class SearchsComponent implements OnInit {
     ).pipe(switchMap((x) => this.api.getCountyDetails(x.Country)));
     this.population$ = this.country$.pipe(map((x) => x[0].population));
     this.population$.pipe(reduce((acc, curr) => acc + curr, 0));
+    */
   }
 }
+
